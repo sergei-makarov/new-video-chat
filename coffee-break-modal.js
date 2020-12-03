cf9bef342e910007b1abba16fa3553c25f2708e6
Makarov Sergei <sergei@miro.com>
2020-12-03 17:09:41 +0500

    kjhbgvfcdxsz`

--- coffee-break-modal.js
+++ coffee-break-modal.js
@@ -376,6 +376,7 @@ var Root = /** @class */ (function (_super) {
         return _this;
     }
     Root.prototype.render = function () {
+        debugger;
         var game = this.state.game;
         return (game ? this.renderGame(game) : this.renderDefault());
     };
